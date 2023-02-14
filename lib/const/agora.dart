afb4a1a1118784dda74300fab45e7a69b2f494b9
ylose <heungeob1003@gmail.com>
2023-02-14 12:25:40 +0900

    아고라 API에서 필요한 상수값 저장(APP_ID, CHANNEL_NAME, TEMP_TOKEN)

---
+++ lib/const/agora.dart
@@ -0,0 +1,3 @@
+const APP_ID = '4559a47e78644ad2ae16ee1c0c9feaf7';
+const CHANNEL_NAME = 'testchannel';
+const TEMP_TOKEN = '007eJxTYFgU68PN/WmTko/M1u/1GzYWfhIpqtOfOItl2mp2kf0cOaoKDCamppaJJuap5hZmJiaJKUaJqYZmqamGyQbJlmmpiWnm3P9eJTcEMjIsLMhgZWSAQBCfm6EktbgkOSMxLy81h4EBAKqtISE=';
