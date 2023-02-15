6dd09d4221bc32cbe209ab535b00e0a4103705c9
ylose <heungeob1003@gmail.com>
2023-02-15 14:34:57 +0900

    first commit

---
+++ lib/screen/cam_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CamScreen extends StatefulWidget {
+  const CamScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CamScreen> createState() => _CamScreenState();
+}
+
+class _CamScreenState extends State<CamScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
