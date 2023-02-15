6c901b51116612eaead6a8dd61e1772b7cc62fcd
ylose <heungeob1003@gmail.com>
2023-02-15 14:27:17 +0900

    화상 통화 시작 버튼 생성

--- lib/entrybutton.dart
+++ lib/entrybutton.dart
@@ -5,6 +5,15 @@ class EntryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        ElevatedButton(
+            onPressed: () {},
+            child: Text('입장하기'),
+        ),
+      ],
+    );
   }
 }
