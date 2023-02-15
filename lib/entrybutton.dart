5e1062a7599629c090808578b9f5aa653e684982
ylose <heungeob1003@gmail.com>
2023-02-15 14:11:09 +0900

    first commit

---
+++ lib/entrybutton.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EntryButton extends StatelessWidget {
+  const EntryButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
