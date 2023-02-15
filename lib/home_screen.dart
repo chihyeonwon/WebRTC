82757fd1b1d7adc9e5d968db0d6bf5f18604f0d3
ylose <heungeob1003@gmail.com>
2023-02-15 13:50:59 +0900

    로고 위젯 추가

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'logo.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -7,6 +9,16 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[100]!,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child:Column(
+            children: [
+              Expanded(child: Logo()), //로고
+            ],
+          ),
+        )
+      ),
     );
   }
 }
