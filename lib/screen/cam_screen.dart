a19f40fee640779dd4a2f8a68aaddf9cc44f2ca4
ylose <heungeob1003@gmail.com>
2023-02-16 13:23:17 +0900

    사용 권한 관리 라이브러리인 permission_handler 사용

--- lib/screen/cam_screen.dart
+++ lib/screen/cam_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class CamScreen extends StatefulWidget {
   const CamScreen({Key? key}) : super(key: key);
