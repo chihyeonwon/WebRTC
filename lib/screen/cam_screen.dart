36de58d41ffb82d40f8c70023d2eee2891003e6e
ylose <heungeob1003@gmail.com>
2023-02-16 13:27:06 +0900

    화상 통화에 필요한 권한(카메라, 마이크)을 받는 init함수 생성

--- lib/screen/cam_screen.dart
+++ lib/screen/cam_screen.dart
@@ -9,6 +9,20 @@ class CamScreen extends StatefulWidget {
 }
 
 class _CamScreenState extends State<CamScreen> {
+
+  Future<bool> init() async {
+    final resp = await [Permission.camera, Permission.microphone].request();
+
+    final cameraPermission = resp[Permission.camera]; // 카메라 권한 요청에 대한 응답을 cameraPermission에 저장
+    final micPermission = resp[Permission.microphone]; // 마이크 권한 요청에 대한 응답을 micPermission에 저장
+
+    if(cameraPermission != PermissionStatus.granted || micPermission != PermissionStatus.granted) {
+      throw '카메라 또는 마이크 권한이 없습니다.';
+    }
+
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
